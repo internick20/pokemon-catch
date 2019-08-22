a5f6bcd83961247dde4462df2c94ae0e75007cea
Jose Trinidad <internick18@gmail.com>
2019-08-22 18:40:44 -0500

    Initial Commit

---
+++ pokemon-catch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemon-catch
+//
+//  Created by Admin on 22/08/19.
+//  Copyright © 2019 galosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
